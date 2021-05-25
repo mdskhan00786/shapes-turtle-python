fabd42a357e4c0456c15d813b838e05e1c3dfd55
mdskhan00786 <mdskhan00786@gmail.com>
2021-05-25 13:40:58 +0530

    commit_name

---
+++ shapes.py
@@ -0,0 +1,29 @@
+import turtle
+
+#circle
+turtle.shape("circle")
+turtle.circle(40)
+
+#square
+turtle.shape("square")
+turtle.forward(80)
+turtle.right(90)
+turtle.forward(80)
+turtle.right(90)
+turtle.forward(80)
+turtle.right(90)
+turtle.forward(80)
+turtle.left(90)
+
+#triangle
+turtle.shape("triangle")
+turtle.forward(120)
+turtle.left(135)
+turtle.forward(170)
+turtle.left(135)
+turtle.forward(120)
+
+#turtle spin
+turtle.right(1440)
+
+
